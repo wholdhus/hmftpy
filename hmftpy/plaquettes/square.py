8b49673f2dd10cc77e84189bdf0c74c9e84d2041
Will Holdhusen <wholdhus@gmail.com>
2022-08-02 09:43:56 -0400

    Working on getting skyrmions on the square lattice

--- hmftpy/plaquettes/square.py
+++ hmftpy/plaquettes/square.py
@@ -12,30 +12,30 @@ Connection diagram for 4-site square (symmetric tiling)
 plaq4 = {'L': 4,
           'inner': {},
           'outer': {}}
-plaq4['inner']['nearest'] = [
-            [1,2], # 0
-            [0,3],
-            [0,3], # 2
-            [1,2]]
-plaq4['inner']['n_nearest'] = [
-            [3], # 0
-            [2],
-            [1], # 2
-            [0]]
-plaq4['outer']['nearest'] = [
-            [1,2], # 0
-            [0,3],
-            [0,3], # 2
-            [1,2]] # 8
-plaq4['outer']['n_nearest'] = [
-            [3],
-            [2],
-            [1],
-            [0]]
-plaq4['inner']['x_bonds'] = [[0,1], [2,3]]
-plaq4['inner']['y_bonds'] = [[0,2], [1,3]]
-plaq4['outer']['x_bonds'] = [[1,0], [3,2]]
-plaq4['outer']['y_bonds'] = [[2,0], [3,1]]
+plaq4['inner']['nearest'] = [[1,2], # 0
+                             [0,3],
+                             [0,3], # 2
+                             [1,2]]
+plaq4['inner']['n_nearest'] = [[3], # 0
+                               [2],
+                               [1], # 2
+                               [0]]
+plaq4['outer']['nearest'] = [[1,2], # 0
+                             [0,3],
+                             [0,3], # 2
+                             [1,2]] # 8
+plaq4['outer']['n_nearest'] = [[3, 3, 3],
+                               [2, 2, 2],
+                               [1, 1, 1],
+                               [0, 0, 0]]
+plaq4['inner']['x_bonds'] = [[0,1], [3,2]]
+plaq4['inner']['y_bonds'] = [[1,3], [2,0]]
+plaq4['outer']['x_bonds'] = [[0,1], [3,2]]
+plaq4['outer']['y_bonds'] = [[1,3], [2,0]]
+# plaq4['inner']['x_bonds'] = [[0,1], [2,3]]
+# plaq4['inner']['y_bonds'] = [[0,2], [1,3]]
+# plaq4['outer']['x_bonds'] = [[1,0], [3,2]]
+# plaq4['outer']['y_bonds'] = [[2,0], [3,1]]
 
 
 """
@@ -53,6 +53,7 @@ Connection diagram for 9-site square (symmetric tiling)
 plaq9 = {'L': 9,
           'inner': {},
           'outer': {}}
+plaq9['inner']['nearest'] = [[]]
 plaq9['inner']['x_bonds'] = [[0,1], [1,2], 
                              [3,4], [4,5],
                              [6,7], [7,8]]
@@ -79,8 +80,8 @@ Connection diagram for 16-site square (symmetric tiling)
 plaq16 = {'L': 16,
           'inner': {},
           'outer': {}}
-plaq16['inner']['x_bonds'] = [[0,1], [1,2], [2,3]
-                              [4,5], [5,6], [6,7]
+plaq16['inner']['x_bonds'] = [[0,1], [1,2], [2,3],
+                              [4,5], [5,6], [6,7],
                               [8,9], [9,10], [10,11],
                               [12,13], [13,14], [14,15]]
 plaq16['inner']['y_bonds'] = [[0,4], [1,5], [2,6], [3,7],
